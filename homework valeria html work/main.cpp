2235ab77d4c1c10040fcfbd70f81c411fa2444d7
valeriabukova <valeriasergeevna688@gmail.com>
2024-12-08 22:34:29 +0200

    Initial Commit

---
+++ homework valeria html work/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  homework valeria html work
+//
+//  Created by Valeria  Bukova on 08.12.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
